3cd61628b9813f05ae6b33bfb8438e184e6f4bcc
Alan <alanleung1909@gmail.com>
2020-10-29 22:50:45 +0800

    Serve static and redirect for invalid route

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8081;
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
 app.use(cors({ origin: process.env.CLIENTURL || 'http://localhost:8080' }));
 app.use(require('./middleware/auth.js'));
 app.use('/api', require('./routes'));
+
+app.get('/*', (req, res) => {
+	res.redirect('/');
+});
 
 mongoose.connect(process.env.DBURL, {
 	useNewUrlParser: true,
